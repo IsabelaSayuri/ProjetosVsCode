cc28d54e464da2da466b2570334c043fadaa8c0e
isabela <isabela.borba@aluno.senai.com.br>
2024-08-07 08:33:42 -0300

    initial

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+const botao = document.getElementById('button');
+
+
+function redirecionar() {
+    window.location.href = 'https://www..com'; // URL da página para onde você deseja redirecionar
+}
+
+
+botao.addEventListener('click', redirecionar);
